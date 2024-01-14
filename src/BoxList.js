2d04bd7e54f6b19293e7c2e93a2c91eacde58783
Aras <vahitaras@gmail.com>
2024-01-14 11:26:15 -0500

    refactored the code

--- src/BoxList.js
+++ src/BoxList.js
@@ -39,36 +39,26 @@ const BoxList = () => {
       <button onClick={handleAdd}>Add a Box</button>
       <AnimatePresence>
         {boxes.map((data, index) => {
-          if (deleteAnimation) {
-            return (
-              <motion.div
-                key={data} // Ensure each box has a unique key
-                layout // Add this prop to enable layout animation
-                layoutTransition={{ duration: 0.05 }} // Optionally, customize the layout transition
-                initial={{ opacity: 1, x: 0 }}
-                exit={{ opacity: 0, x: "-100%" }}
-                // Slide upward Animation on Page Load
-                animate={{ y: 10 }} // Adjust the value based on your preference
-                transition={{ duration: 0.3 }}
-              >
-                <Box data={data} onDelete={() => handleDelete(index)} />
-              </motion.div>
-            );
-          } else {
-            return (
-              <motion.div
-                key={data} // Ensure each box has a unique key
-                layout // Add this prop to enable layout animation
-                layoutTransition={{ duration: 1 }}
-                initial={{ opacity: 0, x: "-100%" }}
-                animate={{ opacity: 1, x: 0, y: 10 }}
-                exit={{ opacity: 0, x: "-100%" }}
-                transition={{ duration: 0.3 }}
-              >
-                <Box data={data} onDelete={() => handleDelete(index)} />
-              </motion.div>
-            );
-          }
+          return (
+            <motion.div
+              key={data} // Ensure each box has a unique key
+              layout // Add this prop to enable layout animation
+              layoutTransition={{ duration: 0.05 }} // Optionally, customize the layout transition
+              initial={
+                deleteAnimation
+                  ? { opacity: 1, x: 0 }
+                  : { opacity: 0, x: "-100%" }
+              }
+              exit={{ opacity: 0, x: "-100%" }}
+              // Slide upward Animation on Page Load
+              animate={
+                deleteAnimation ? { y: 10 } : { opacity: 1, x: 0, y: 10 }
+              } // Adjust the value based on your preference
+              transition={{ duration: 0.3 }}
+            >
+              <Box data={data} onDelete={() => handleDelete(index)} />
+            </motion.div>
+          );
         })}
       </AnimatePresence>
     </div>
